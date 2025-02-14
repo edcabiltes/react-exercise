e981ce5d4d9fcdf6e1d324103d9403417c8c6917
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-14 17:23:31 +0800

    Handled controlled inputs in forms

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,38 @@
+import { useState } from "react"
+
 const Create = () => {
+  const [title, setTitle] = useState(``);
+  const [body, setBody] = useState(``);
+  const [author, setAuthor] = useState(`ed`);
 
   return (
     <div className="create">
         <h2>Add a New Blog</h2>
+        <form>
+          <label>Blog title:</label>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Blog body:</label>
+          <textarea
+            type=""
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <label>Blog author:</label>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value="ed">ed</option>
+            <option value="baste">baste</option>
+          </select>
+          <button>Add Blog</button>
+        </form>
     </div>
   )
 }
