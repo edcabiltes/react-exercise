173568c0f4f68aa3a026abe01d167df70a9b226d
Ed Cabiltes <edcabiltes@outlook.com>
2025-02-14 20:49:09 +0800

    Implemented programmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState(``);
   const [body, setBody] = useState(``);
   const [author, setAuthor] = useState(`ed`);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,6 +18,7 @@ const Create = () => {
       body: JSON.stringify(blog)
     }).then(() => {
       console.log(`new blog added`);
+      navigate(`/`);
     })
   }
 
